c92e42b6f0f79127862473db3a3b6e64258fdf11
antoniomasgut <antoniomasgut@gmail.com>
2022-11-06 10:31:51 +0100

    Initial commit

---
+++ src/main/java/org/daw/apiservlet/webapp/headers/controllers/HoraActualizadaServlet_5.java
@@ -0,0 +1,37 @@
+package org.daw.apiservlet.webapp.headers.controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+@WebServlet("/hora-actualizada")
+public class HoraActualizadaServlet_5 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        resp.setHeader("refresh", "1"); // La pàgina s'ha de refescar cada segon
+        LocalTime hora = LocalTime.now();
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("hh:mm:ss");
+        try (PrintWriter out = resp.getWriter()) {
+
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("    <head>");
+            out.println("        <meta charset=\"UTF-8\">");
+            out.println("        <title>La hora actualizada</title>");
+            out.println("    </head>");
+            out.println("    <body>");
+            out.println("        <h1>La hora actualizada!</h1>");
+            out.println("<h3>"+ hora.format(df) + "</h3>");//Li dona el format df a l'hora
+            out.println("    </body>");
+            out.println("</html>");
+        }
+    }
+}
